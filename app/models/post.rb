129645395a2d07a93352ee10d5c62584feaf216a
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 15:41:40 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates_numericality_of :content, {greater_than_or_equal_to: 250 }
-  validates :summary, length: { maxminum: 250}
+  validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
 end
+
+Range unspecified. Specify the :in, :within, :maximum, :minimum, or :is option.
