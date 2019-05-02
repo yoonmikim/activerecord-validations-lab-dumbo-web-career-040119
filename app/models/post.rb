404691e9fd341eee5842d3a5942338babd04c3e3
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 15:44:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: { mimimum: 250}
+  validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
 end
