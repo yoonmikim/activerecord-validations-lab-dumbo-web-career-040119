c44eb60f078cbf425667e04c3d207325a983ed7e
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 15:45:54 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,3 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
 end
-Failure/Error: expect(non_clickbait).to be_invalid
-       expected `#<Post id: nil, title: "True Facts", content: "Lorem ipsum dolor sit amet, consectetur adipiscing...", summary: "Abstract.", category: "Fiction", created_at: nil, updated_at: nil>.invalid?` to return true, got false
-     
