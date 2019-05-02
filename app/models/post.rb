7876a1a34d300c87f4fecfe9fbb709962ef6f149
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 15:49:19 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,18 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
-  validates :title, inclusion: { in: [ "Won't Believe", "Secret", "Top [number]", "Guess"]}
+  validate :is_clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  # def is_clickbait?
+  #   if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+  #     errors.add(:title, "must be clickbait")
+  #   end
+  # end
 end
