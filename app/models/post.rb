99e0ba8dbdb9db248a9a4f28519a8d4edbad618e
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 15:48:07 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
-  validates :title, inclusion: { in: [ "Won't Believe", "Secret", "Top [#{number}]", "Guess"]}
+  validates :title, inclusion: { in: [ "Won't Believe", "Secret", "Top [number]", "Guess"]}
 end
